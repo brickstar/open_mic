6bc031f06358fdd7e4f68582212e81b67b5f9fca
brickstar <brickstar@gmail.com>
2018-02-25 10:52:18 -0700

    initialized Joke, first test passed

--- test/joke_test.rb
+++ test/joke_test.rb
@@ -4,5 +4,9 @@ require 'minitest/pride'
 require './lib/joke'
 
 class JokeTest < Minitest::Test
+  def test_it_exists
+    joke = Joke.new({id: 1, question: "Why did the strawberry cross the road?", answer: "Because his mother was in a jam."})
 
+    assert_instance_of Joke, joke
+  end
 end
